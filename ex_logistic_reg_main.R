eb5f935d7ba3f343579a216ab76a1d4bf2f91278
Sridharan Venkataramanan <v-sridve@microsoft.com>
2015-11-20 12:41:12 +0530

    Updated logistic with reg

--- ex_logistic_reg_main.R
+++ ex_logistic_reg_main.R
@@ -1,3 +1,22 @@
+source("ex_logistic_main.r")
+
+featureExpansion <- function(X,deg)
+{
+  X <- polym(X[,1],X[,2],raw = T,degree = deg)
+  X <- cbind(1,X)
+  X
+}
+
+logisticRegCost <- function(theta,X,y, lambda)
+{
+  cost <- logisticCost(theta,X,y)
+  theta <- as.matrix(theta)[,-1]
+  m <- length(y)
+  theta <-  theta ^ 2;
+  costReg <- cost + (lambda/(2*m)) * sum(theta)
+  costReg
+}
+
 exec_logistic_reg <- function()
 {
   csvData <- read.csv("ex2data2.csv",header = F)
@@ -6,7 +25,12 @@ exec_logistic_reg <- function()
   csvData <- as.matrix(csvData)
   colLentgth <- dim(csvData)[2]
   X <- csvData[,c(1:(colLentgth-1))]
-  X <- cbind(1,X)
+  X <- featureExpansion(X,6)
+  write.csv(X,file="expanded.csv");
   Y <- csvData[,colLentgth]
   initial_theta <- c(rep(0,dim(X)[2]))
+  lambda <- 1
+  costReg <- logisticRegCost(initial_theta,X,Y,lambda)
+  
+  costReg
 }
