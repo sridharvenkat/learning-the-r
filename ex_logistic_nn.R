ddf64e24a84808a7839a7503bc15d9dabc6541f8
Sridharan Venkataramanan <v-sridve@microsoft.com>
2015-11-26 15:59:44 +0530

    Updated the code with new logic

--- ex_logistic_nn.R
+++ ex_logistic_nn.R
@@ -2,16 +2,21 @@ source("ex_logistic_reg_main.r")
 
 exec_logistic_nn <- function()
 {
-  X <- read.csv(".\\data\\ex_logistic_nn_x.csv", header = F)
+  X <- as.matrix(read.csv(".\\data\\ex_logistic_nn_x.csv", header = F))
   Y <- read.csv(".\\data\\ex_logistic_nn_y.csv", header = F)
-  
+  Y <- as.numeric(Y$V1)
+  X <- cbind(1,X)
   yLevels <- as.numeric(levels(factor(Y)))
-  
+  optimSet <- matrix(nrow=length(yLevels),ncol = dim(X)[2])
   for(yLevel in yLevels)
   {
     logicalY <- as.numeric(Y == yLevel)
-    theta <- c(rep(0,length(Y)))
+    initial_theta <- c(rep(0,dim(X)[2]))
     lambda <- 0.1
-    optimal <- optim(theta,logisticRegCost,logisticRegGradient,X=X,y=Y, lambda = lambda, method = "BFGS")  
+    optimal <- optim(initial_theta,logisticRegCost,logisticRegGradient,X=X,y=logicalY, lambda = lambda, method = "BFGS")  
+    optimSet[yLevel,] = optimal$par
   }
+  prediction <- X %*% t(optimSet)
+  predictedResult <- apply(prediction,1,which.max)
+  mean(predictedResult == Y) * 100
 }
